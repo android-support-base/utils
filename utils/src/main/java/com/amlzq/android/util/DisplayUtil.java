db1c92fe85e76b94f23875f5c1fb5885fde1ba93
amlzq <lzqjiujiang@gmail.com>
2019-10-09 17:21:06 +0800

    change DisplayUtil

--- utils/src/main/java/com/amlzq/android/util/DisplayUtil.java
+++ utils/src/main/java/com/amlzq/android/util/DisplayUtil.java
@@ -1,4 +1,4 @@
-package com.amlzq.android.util;
+package com.esports.lib_common_module.utils;
 
 import android.content.Context;
 import android.content.res.Configuration;
@@ -7,9 +7,9 @@ import android.util.DisplayMetrics;
 /**
  * Created by amlzq on 2017/7/28.
  * <p>
- * 屏幕显示工具类
+ * 显示指标工具
  */
-// @SuppressWarnings("unused")
+
 public class DisplayUtil {
 
     /**
@@ -23,6 +23,14 @@ public class DisplayUtil {
     // =============================================================================================
 
     /**
+     * 密度
+     */
+    public static float DENSITY = -1.0F;
+    /**
+     * 密度
+     */
+    public static float SCALED_DENSITY = -1.0F;
+    /**
      * 屏幕宽度，单位像素
      */
     public static int SCREEN_WIDTH = -1;
@@ -31,28 +39,25 @@ public class DisplayUtil {
      */
     public static int SCREEN_HEIGHT = -1;
     /**
-     * 密度
-     */
-    private static float DENSITY = -1.0F;
-    /**
      * X轴缩放比例
      */
-    private static float SCALE_X = -1.0F;
+    public static float SCALE_X = -1.0F;
     /**
      * Y轴缩放比例
      */
-    private static float SCALE_Y = -1.0F;
+    public static float SCALE_Y = -1.0F;
 
     public static void init(Context context) {
-        DisplayMetrics dm = context.getResources().getDisplayMetrics();
-        SCREEN_WIDTH = dm.widthPixels;
-        SCREEN_HEIGHT = dm.heightPixels;
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        DENSITY = metrics.density;
+        SCALED_DENSITY = metrics.scaledDensity;
+        SCREEN_WIDTH = metrics.widthPixels;
+        SCREEN_HEIGHT = metrics.heightPixels;
         if (SCREEN_WIDTH > SCREEN_HEIGHT) {
             int i = SCREEN_WIDTH;
             SCREEN_WIDTH = SCREEN_HEIGHT;
             SCREEN_HEIGHT = i;
         }
-        DENSITY = dm.density;
         SCALE_X = SCREEN_WIDTH / 480.0F;
         SCALE_Y = SCREEN_HEIGHT / 800.0F;
 
@@ -64,63 +69,50 @@ public class DisplayUtil {
          */
     }
 
-    public static int getScreenWidth(Context context) {
-        if (SCREEN_WIDTH > 0) {
-            return SCREEN_WIDTH;
-        }
-        init(context);
-        return SCREEN_WIDTH;
-    }
-
-    public static int getScreenHeight(Context context) {
-        if (SCREEN_HEIGHT > 0) {
-            return SCREEN_HEIGHT;
-        }
-        init(context);
-        return SCREEN_HEIGHT;
-    }
-
-    // 转换dip为px
-    public static int dip2px(Context context, int dip) {
-        float scale = context.getResources().getDisplayMetrics().density;
+    /**
+     * @param dip
+     * @return 转换dip为px
+     */
+    public static int dip2px(int dip) {
+        float scale = DENSITY;
         return (int) (dip * scale + 0.5f * (dip >= 0 ? 1 : -1));
     }
 
-    // 转换px为dip
-    public static int px2dip(Context context, int px) {
-        float scale = context.getResources().getDisplayMetrics().density;
+    /**
+     * @param px
+     * @return 转换px为dip
+     */
+    public static int px2dip(int px) {
+        float scale = DENSITY;
         return (int) (px / scale + 0.5f * (px >= 0 ? 1 : -1));
     }
 
-    // 转换sp为px
-    public static int sp2px(Context context, float spValue) {
-        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
-        return (int) (spValue * fontScale + 0.5f);
+    /**
+     * @param sp
+     * @return 转换sp为px
+     */
+    public static int sp2px(float sp) {
+        float fontScale = SCALED_DENSITY;
+        return (int) (sp * fontScale + 0.5f);
     }
 
-    // 转换px为sp
-    public static int px2sp(Context context, float pxValue) {
-        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
-        return (int) (pxValue / fontScale + 0.5f);
+    /**
+     * @param px
+     * @return 转换px为sp
+     */
+    public static int px2sp(float px) {
+        float fontScale = SCALED_DENSITY;
+        return (int) (px / fontScale + 0.5f);
     }
 
-    public static int getInt(Context context, int paramInt) {
-        return getIntForScalX(context, paramInt);
+    public static int getInt(int paramInt) {
+        return getIntForScaleX(paramInt);
     }
 
-    private static int getIntForScalX(Context context, int paramInt) {
-        return (int) (paramInt * getScaleX(context));
+    private static int getIntForScaleX(int paramInt) {
+        return (int) (paramInt * SCALE_X);
     }
 
-    public static float getScaleX(Context context) {
-        if (SCALE_X > 0.0F) {
-            return SCALE_X;
-        }
-        init(context);
-        return SCALE_X;
-    }
-
-    @SuppressWarnings("deprecation")
     public static String getOrientationName(int orient) {
         String orientName;
         switch (orient) {
